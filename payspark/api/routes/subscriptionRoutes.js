0ffe7d6f469c763011dfa073fe7e779331954d80
Kiffeur <emmanueldesong46@gmail.com>
2024-08-04 22:36:38 +0200

    clean project

--- payspark/api/routes/subscriptionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const subscriptionController = require('../controllers/subscriptionController');
-
-const router = express.Router();
-
-router.post('/:id/subscribe', subscriptionController.subscribeUserToPlan);
-//router.get('/:id/subscriptions', userSubscriptions.userSubscriptions);
-
-module.exports = router;
